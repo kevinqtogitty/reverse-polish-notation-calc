a87c74bd70ec20812c678057b7161408f0a93c79
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-05-17 00:42:09 -0400

    Update: added object type

---
+++ src/utils/operations.ts
@@ -0,0 +1,8 @@
+import { CalcFuntions } from '../types/types';
+
+export const operations: CalcFuntions = {
+  '+': (a: number, b: number) => a + b,
+  '-': (a: number, b: number) => a - b,
+  '*': (a: number, b: number) => a * b,
+  '/': (a: number, b: number) => a / b
+};
